fd3463843f67e80bc8686e65c4f2aef935f80858
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-14 00:23:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,8 @@ class App < Sinatra::Base
   end
 
   post "/multiply/:num1/:num2" do
-    @numbers = params[:num1, :num2]
-    "#{num1 * num2}"
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    "#{@num1 * @num2}"
   end
 end
